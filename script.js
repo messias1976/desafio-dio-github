3d63937d9c4ab333e0701a5c1e40f6ca5e452f46
Messias Machado <messiasmachado1976@gmail.com>
2022-04-25 21:42:18 -0300

    estrutura inicial de arquivos

---
+++ script.js
@@ -0,0 +1,18 @@
+function getAdmins(map){
+    let admins =[];
+    for ([key, value] of map) {
+        if (value === 'Admin') {
+            admins.push(key)
+        }
+    }
+    return admins;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luis', 'Admin');
+usuarios.set('Erik', 'Admin');
+usuarios.set('Marcelo', 'User');
+usuarios.set('Mirela', 'Admin');
+
+console.log (getAdimns(usuarios));
